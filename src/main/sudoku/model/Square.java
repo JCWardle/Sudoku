19e2dcb7cebe3586cba77321d182b9b5b659bd5d
jordanw <jordan@ninjatertle.com>
2014-12-03 15:23:11 +1100

    Implemented the square class.

--- src/main/sudoku/model/Square.java
+++ src/main/sudoku/model/Square.java
@@ -3,10 +3,17 @@ package main.sudoku.model;
 import java.util.List;
 
 public class Square {
+    private List<List<Cell>> _cells;
+
     public Square(List<List<Cell>> cells){
+        _cells = cells;
     }
 
     public boolean isCellValueValid(int x, int y, Cell value){
-        return false;
+        for (List<Cell> row : _cells) {
+            if (row.contains(value))
+                return false;
+        }
+        return true;
     }
 }
